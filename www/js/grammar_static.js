c7525b97d3b0bf75929a8822d9a968834ecdb1d4
Rob Bates <tacobates@gmail.com>
2016-10-17 15:59:55 -0600

    Add files via upload

---
+++ www/js/grammar_static.js
@@ -0,0 +1,13 @@
+var gramStat = [
+"Calm Down, Grandma",
+"You Wish, Grandpa",
+"Llama Stank",
+"Under the Gilded Firmament",
+"Legally Distinct Entity",
+"Compromise Name",
+"Version 1.1",
+"Just got Mugged",
+"",
+"",
+"",
+];
